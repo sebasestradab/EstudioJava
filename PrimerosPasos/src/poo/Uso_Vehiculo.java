fa369e177cfba31aac04136a2253b84661340cdc
sebasestradab <31464144+sebasestradab@users.noreply.github.com>
2017-08-31 23:16:09 -0500

    Clase Herencia de Java

--- PrimerosPasos/src/poo/Uso_Vehiculo.java
+++ PrimerosPasos/src/poo/Uso_Vehiculo.java
@@ -1,16 +1,29 @@
 //===============================
 // Tutorial de Java YouTube
 // CLASE: Uso_Coche
-// Video 28-29-30-31-32
+// Video 28-29-30-31-32-41
 //===============================
 package poo;
 
-import javax.swing.*;
+//import javax.swing.*;
 
-public class Uso_Coche {
+public class Uso_Vehiculo {
 
 	public static void main(String[] args) {
-		Coche micoche = new Coche(); // Instanciar una clase - Renault: Ejemplar de Clase
+		Coche micoche1 = new Coche();
+		Furgoneta mifurgoneta1 = new Furgoneta(7, 580);
+				
+		micoche1.establece_color("Rojo");
+		
+		//mifurgoneta1.establece_color("Azul");
+		//mifurgoneta1.configura_asientos("Si");
+		//mifurgoneta1.configura_climatizador("Si");
+		
+		System.out.println(micoche1.dime_datos_generales() + " " + micoche1.dime_color());
+		System.out.println(mifurgoneta1.dime_datos_generales() + " " + mifurgoneta1.dimeDatosFurgoneta());
+		
+		///// Borrado ////////////////
+		/*Coche micoche = new Coche(); // Instanciar una clase - Renault: Ejemplar de Clase
 		//System.out.println("Este coche tiene " + Renault.ruedas + " ruedas.");
 				
 		micoche.establece_color(JOptionPane.showInputDialog("Introduce el color"));
@@ -25,7 +38,8 @@ public class Uso_Coche {
 		
 		System.out.println(micoche.dime_peso_coche());
 		
-		System.out.println("El precio final del coche es " + micoche.precio_coche());
+		System.out.println("El precio final del coche es " + micoche.precio_coche());*/
+		
 	}
 
 }
